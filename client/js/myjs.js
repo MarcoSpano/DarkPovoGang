85e3be0b92ac452895f479627fb9254f20e80921
gioferro96 <giovanni.ferronato@studenti.unitn.it>
2017-11-24 12:34:44 +0100

    word check + popUp if wrong input

--- client/js/myjs.js
+++ client/js/myjs.js
@@ -1,7 +1,27 @@
+
+var possibilities = ["povo","economia","lettere","filosofia","mesiano",
+					"ingegneria","giurisprudenza","sociologia","scienze cognitive",
+					"giuri","socio","help"];
+
+function contains(arr, element) {
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] === element) {
+		return true;
+		}
+	}
+	return false;
+}
+
+function delay(t) {
+	return new Promise(function(resolve) { 
+		setTimeout(resolve, t)
+	});
+}
+
 function showCommand(){
 	console.log("showCommand")
 	var table = document.getElementById("table_div");
-	table.style.visibily = "visible"
+	table.style.visibility = "visible";
 
 }
 
@@ -26,11 +46,17 @@ function go(){
 function getQueryVariable_q(url_string) {
 	var url = new URL(url_string);
 	var query = url.searchParams.get("q");
+	console.log("getQueryVariable_q = "+query);
 	if(query)
-		return query;
+		if(contains(possibilities,query))
+			return query;
+		else if (query === "dc")
+			return query;
+		else
+			return null;
 	return null;
-	//alert('Query Variable ' + query + ' not found');
 }
+
 function getQueryVariable_geoloc(url_string) {
 	var url = new URL(url_string);
 	var query = url.searchParams.get("geoloc");
@@ -39,3 +65,4 @@ function getQueryVariable_geoloc(url_string) {
 	return null;
 	//alert('Query Variable ' + query + ' not found');
 }
+
