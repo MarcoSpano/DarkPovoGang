1d1d7e926a0e41c12848276efb32ad819c56ebfb
Beppolt <giuseppespallitta@yahoo.it>
2017-11-29 21:06:52 +0100

    Added function to manage Mesiano maps on Telegram

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const path = require("path");
 const request = require('request');
 const cors = require('cors');
 const fetch = require("node-fetch");
-const mapper = require('./server/utilities');
+const mapper = require('./server/utilities.js');
 const svg2png=require('svg2png');
 const unirest=require('unirest');
 const q=require('q');
-
 
 var povo1p1 = '/img/Povo1P1.svg';
 var povo1pt = '/img/Povo1PT.svg';
@@ -24,7 +23,7 @@ function getData(sede){
 	let month = now.getMonth() + 1;
 	let year = now.getFullYear();
 	let currentTimestamp = now.getTime() / 1000;
-	url = "https://easyroom.unitn.it/Orario/rooms_call.php?form-type=rooms&sede="+sede+"&_lang=it&date=" + day + "-" + month + "-" + year;
+	url = "https://easyroom.unitn.it/Orario/rooms_call.php?form-type=rooms&sede="+sede+"&_lang=it&date=30-" + month + "-" + year;
 	return fetch(url)
 	.then(body => {
 			return body.json();
@@ -38,6 +37,7 @@ function getData(sede){
 			rooms = mapper.cleanSchedule(rooms);    
 			rooms = mapper.getFreeRooms(rooms, currentTimestamp);
 			rooms = mapper.cleanPastSchedule(rooms, currentTimestamp);
+			console.log(rooms);
 			return rooms;
 	})
 	.catch(error => {
@@ -45,7 +45,7 @@ function getData(sede){
 	});
 }
 
-function getDataAndMaps(sede, id){
+function getDataAndMaps(sede, id, value){
 
 	let rooms = [];
 	let now = new Date();
@@ -64,16 +64,17 @@ function getDataAndMaps(sede, id){
 			return data.events;
 	})
 	.then(events => {
+			
 			console.log("SECONDO .then");
 			rooms = mapper.getRoomList(events); 
 			rooms = mapper.cleanSchedule(rooms);    
 			rooms = mapper.getFreeRooms(rooms, currentTimestamp);
 			rooms = mapper.cleanPastSchedule(rooms, currentTimestamp);
-			//console.log(rooms);
+			console.log(rooms);
 			return rooms;
 	})
 	.then(rooms => {
-		var maps = mapper.getMaps(rooms,sede);
+		var maps = mapper.getMaps(rooms,sede,value);
 		//console.log(maps);
 		return maps;
 	})
@@ -101,6 +102,7 @@ function Print(sede,chatid){
         let msg = "";
 		//var maps = getDataAndMaps(sede, chatid);
 		let rooms = getData(sede).then(rooms => {
+
 			for(let i = 0; i < rooms.length; i++){
 				for(let j = 0; j < rooms[i].orario.length; j++){
 					msg += rooms[i].NomeAula+" libera fino alle "+rooms[i].orario[j].from+"\n";
@@ -128,8 +130,18 @@ telegram.on("text", (message) => {
 	}
 	else if (message.text == "/help")
 	{
-		telegram.sendMessage(message.chat.id, "I comandi disponibili sono: \n/help \n/start \n/povo \n/socio \n/economia \n/scicogn \n/lettere \n/giuri \n/mesiano \n/filosofia"); 
+		telegram.sendMessage(message.chat.id, "I comandi disponibili sono: \n/help \n/start \n/povo \n/socio \n/economia \n/scicogn \n/lettere \n/giuri \n/mesiano \n/filosofia \n/mappePovo \n/mappeMesiano"); 
 	}
+	else if (message.text == "/mappePovo"){
+    	for(let i=0; i < 4;i++){
+    		getDataAndMaps("E0503",message.chat.id,i);
+    	}
+    }
+    else if (message.text == "/mappeMesiano"){
+    	for(let i=0; i < 4;i++){
+    		getDataAndMaps("E0301",message.chat.id,i);
+    	}
+    }
 	else if (message.text.toLowerCase().includes("povo"))Print("E0503",message.chat.id);
 	else if (message.text.toLowerCase().includes("ingegneria") || message.text.toLowerCase().includes("mesiano"))Print("E0301",message.chat.id);
 	else if (message.text.toLowerCase().includes("giurisprudenza") || message.text.toLowerCase().includes("giuri"))Print("E0201",message.chat.id);
@@ -137,7 +149,7 @@ telegram.on("text", (message) => {
 	else if (message.text.toLowerCase().includes("filosofia") || message.text.toLowerCase().includes("lettere"))Print("E0801",message.chat.id);
 	else if (message.text.toLowerCase().includes("scienze cognitive") || message.text.toLowerCase().includes("scicogn"))Print("E0705",message.chat.id);
 	else if (message.text.toLowerCase().includes("economia"))Print("E0101",message.chat.id);
-    else if (message.text == "/mappe")telegram.sendPhoto(message.chat.id,photo)
+    
 	else
     {
 		telegram.sendMessage(message.chat.id,"Comando non riconosciuto! Digita /help per conoscere la lista dei comandi.")
